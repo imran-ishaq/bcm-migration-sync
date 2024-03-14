6e27c4db16a91e188771ef515be8af61798d21ef
imran-ishaq <imranishaq7071@gmail.com>
2024-03-14 15:07:31 +0500

    update the migration controller

--- src/main/java/com/itmaxglobal/bcmmigrationsync/controller/MigrationController.java
+++ src/main/java/com/itmaxglobal/bcmmigrationsync/controller/MigrationController.java
@@ -3,18 +3,18 @@ package com.itmaxglobal.bcmmigrationsync.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @RestController
-@RequestMapping(value = "/api/migration")
+@RequestMapping()
 @Slf4j
 public class MigrationController {
 
-    @PostMapping(value = "/status")
+    @GetMapping(value = "/status")
     public ResponseEntity<?> status(){
         return ResponseEntity.status(HttpStatus.OK).body(new HashMap<>().put("message","Application is running..."));
     }
